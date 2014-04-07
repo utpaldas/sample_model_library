dcde84dc5790e95d79f438f752f3177ed4bcfb92
utpaldas <utpaldas@gmail.com>
2014-04-07 12:03:08 -0400

    Updates to employee model

--- src/main/java/com/sample/model/Employee.java
+++ src/main/java/com/sample/model/Employee.java
@@ -5,8 +5,10 @@ public class Employee extends Person {
 	private long employeeId;
 
 	private EmployeeTypeEnum role;
-	
+
 	private Address address;
+
+	private String dept;
 
 	public Employee() {
 		// TODO Auto-generated constructor stub
@@ -27,13 +29,21 @@ public class Employee extends Person {
 	public void setRole(EmployeeTypeEnum role) {
 		this.role = role;
 	}
-	
+
 	public Address getAddress() {
 		return address;
 	}
-	
+
 	public void setAddress(Address address) {
 		this.address = address;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
 	}
 
 }
